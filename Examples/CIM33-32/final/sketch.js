de1db9cb9d09c6ba31872f615fd8d84209ea9cc3
txb450 <txb450@miami.edu>
2019-12-09 12:03:25 -0500

    final

--- Examples/CIM33-32/final/sketch.js
+++ Examples/CIM33-32/final/sketch.js
@@ -1,5 +1,12 @@
 var happy;
 var playlist;
+var sad;
+var mellow;
+var angry;
+var grinding;
+
+
+
 
 function setup() {
   // put setup code here
@@ -7,8 +14,8 @@ function setup() {
 
   happy = new Clickable();     //Create button
 happy.locate(50, 50);        //Position Button
-happy.resize(300,300);      // width height
-happy.color = "green";
+happy.resize(100,100);      // width height
+happy.color = "Orange";
 happy.text = "happy";
 happy.onPress = function(){  //When myButton is pressed
   // happy.color = "black";       //Change button color
@@ -23,17 +30,79 @@ happy.onPress = function(){  //When myButton is pressed
     }
 
     happy.onHover = function(){  //When myButton is pressed
-      happy.color = "red";       //Change button color
+      happy.color = "gold";       //Change button color
       //do an action
       }
 
       happy.onOutside = function(){  //When myButton is pressed
-        happy.color = "blue";       //Change button color
+        happy.color = "orange";       //Change button color
         //do an action
         }
 
         playlist = createSpan();
+
+        sad = new Clickable();
+ sad.locate(150, 50);        //Position Button
+ sad.resize(100,100);      // width height
+ sad.color = "blue";
+ sad.text = "sad";
+ sad.onPress = function(){
+
+   playlist.html('<iframe src="https://open.spotify.com/embed/playlist/6vbetH32Fk2oRjeznWkKtx" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>')
+
 }
+
+sad.onHover = function(){
+  sad.color = "navy blue";
+}
+sad.onOutside = function(){
+  sad.color = "blue";
+}
+      mellow = new Clickable();
+mellow.locate(250,50);
+mellow.resize(100,100);
+mellow.color = "yellow";
+mellow.text = "mellow";
+mellow.onPress = function(){
+  playlist.html('<iframe src="https://open.spotify.com/embed/playlist/1w4ewZUfzwa8eyWkQAf8WY" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>')
+}
+
+mellow.onHover = function(){
+  mellow.color = "gold";
+}
+mellow.onOutside = function(){
+  mellow.color = "yellow"
+}
+      angry = new Clickable();
+angry.locate(350,50);
+angry.resize(100,100);
+angry.color = "red";
+angry.text = "angry";
+angry.onPress = function(){
+  playlist.html('<iframe src="https://open.spotify.com/embed/playlist/7L08IETH8EQmm7k4r8rivb" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>')
+}
+angry.onOutside = function(){
+  angry.color = "red";
+}
+angry.onHover = function(){
+  angry.color = "white"
+}
+
+        grinding = new Clickable();
+ grinding.locate(450,50);
+ grinding.resize(100,100);
+ grinding.color = "green";
+ grinding.text = "grinding";
+ grinding.onPress = function(){
+   playlist.html('<iframe src="https://open.spotify.com/embed/playlist/5iQ0qBTxtuSZYsVDsBYxHQ" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>')
+ }
+ grinding.onOutside = function(){
+   grinding.color = "green"
+ }
+ grinding.onHover = function(){
+   grinding.color = "white"
+ }
+}//functionsetup end
 
 function draw() {
   // put drawing code here
@@ -41,9 +110,12 @@ function draw() {
   background("black");
 
   fill("white");
-  text("stuff goes here", 50,20);
+  text("Explore how you feel! Let the music guide your emotions!", 300,20);
 
   happy.draw();
-
+  sad.draw();
+  mellow.draw();
+  angry.draw();
+  grinding.draw();
 
 }
